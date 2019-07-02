38b484bf24ec4c0c48352a87b59718f203815a23
sankarpv <sankar.narain87@gmail.com>
2019-07-02 22:28:42 +0530

    new Rest

--- src/main/java/com/sankar/employee/rest/EmployeeControl.java
+++ src/main/java/com/sankar/employee/rest/EmployeeControl.java
@@ -1,6 +1,10 @@
 package com.sankar.employee.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +12,12 @@ public class EmployeeControl {
 	@GetMapping("/")
 	public String test(){
 		return "Spring boot Rest";
+	}
+	
+	@RequestMapping(value = "emp/", method=RequestMethod.GET)
+	public ResponseEntity<Employee> getEmployee(){
+		return new ResponseEntity<Employee>(new Employee(1,"SANKAR","JAVA RESOURCE",10000), 
+				HttpStatus.OK);
+		
 	}
 }
